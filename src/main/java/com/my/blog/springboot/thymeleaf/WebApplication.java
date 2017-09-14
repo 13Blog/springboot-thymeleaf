5d60b424b225e8d937a4b2854007a8d8b35358a0
13 <1034683568@qq.com>
2017-09-14 13:23:05 +0800

    :tada: Springboot+Thymeleaf

---
+++ src/main/java/com/my/blog/springboot/thymeleaf/WebApplication.java
@@ -0,0 +1,23 @@
+package com.my.blog.springboot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * author:13
+ * date:2017-09-14
+ */
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
+
---
+++ src/main/java/com/my/blog/springboot/thymeleaf/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.my.blog.springboot.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * author:13
+ * date:2017-09-14
+ */
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(HttpServletRequest request, @RequestParam(value = "name", required = false, defaultValue = "springboot-thymeleaf") String name) {
+        request.setAttribute("name", name);
+        return "hello";
+    }
+}
